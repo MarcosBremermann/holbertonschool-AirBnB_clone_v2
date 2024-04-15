d4eb43f6c276557af7628f09ba026dca5174a9bf
MarcosBremermann <mabm2004@gmail.com>
2024-04-14 23:47:07 -0300

    Task 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""
-script that starts a Flask web application
-"""
+"""Starts a Flask web application"""
+
 from flask import Flask
 
-
 app = Flask(__name__)
-HOST = "0.0.0.0"
-PORT = 5000
+
+
 @app.route('/', strict_slashes=False)
-def norm_return():
-    return 'Hello HBNB'
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
-    app.run(HOST, PORT)
+    app.run(host='0.0.0.0', port='5000')
